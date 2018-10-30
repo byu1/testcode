41ccae4490e7dedb850f604489f5390eec36eb07
Bo YU <b.yu@imperial.ac.uk>
2018-10-30 12:02:52 +0000

    add file

---
+++ mysoftware/test.py
@@ -0,0 +1,6 @@
+from mysoftware import *
+
+def test_square_integers():
+	assert square(2) == 4
+	assert square(0) == 0
+	assert square(-1) == 1
---
+++ test_mysoftware.py
@@ -0,0 +1,7 @@
+from mysoftware import *
+
+def test_square_integers():
+	assert square(2) == 4
+	assert square(0) == 0
+	assert square(-1) == 1
+	assert square(3) == 9
